18995212216d36a2eb99f0cbd5201c0177b6b6a9
baxterjo <me@baxterjo.dev>
2026-08-11 12:46:41 -0700

    chore: Fixed link

--- babel-proto/src/data_types/mod.rs
+++ babel-proto/src/data_types/mod.rs
@@ -2,7 +2,7 @@
 
 /// Interval as defined in section [4.1.2](https://datatracker.ietf.org/doc/html/rfc8966#name-interval)
 ///
-/// This is a re-export alias of [`utils::Duration`] to align with the terminology of the Babel spec.
+/// This is a re-export alias of [`crate::utils::Duration`] to align with the terminology of the Babel spec.
 pub use crate::utils::time::Duration as Interval;
 
 /// Addresses as described in section [4.1.4 and 4.1.5](https://datatracker.ietf.org/doc/html/rfc8966#name-address)
